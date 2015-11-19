ae890182610be6e7b1ffce2e21484bf3e56ed99d
Aravind Srivatsan <asrivatsan@paypal.com>
2015-11-19 20:33:34 +0530

    :page_with_curl: Page scrolling added

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,39 @@
 'use strict';
 
-requirejs.config({
-    paths: {}
-});
-
-require([], function() {
+require(['config'], function(config) {
+  require(['jquery'], function($, sticky) {
     var app = {
-        initialize: function() {}
+      initialize: function() {
+        $(document).ready(function(e) {
+          var viewportHeight = $(window).height();
+          var navHeight = $('nav').height();
+          $(window).resize(function() {
+            viewportHeight = $(window).height();
+          });
+
+          $(document).on('scroll', function(e) {
+            if ($('body').scrollTop() > viewportHeight - navHeight) {
+              $('nav').addClass('fixed');
+            } else {
+              $('nav').removeClass('fixed');
+            }
+        });
+
+          $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              if (target.length) {
+                $('html,body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });
+        });
+      }
     };
     app.initialize();
+  });
 });
---
+++ public/js/config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+requirejs.config({
+  waitSeconds: 0,
+  paths: {
+    jquery: '../components/jquery/dist/jquery.min',
+  }
+});
