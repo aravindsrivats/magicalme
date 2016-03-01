ac3f95cb2c6864dd5aca96d822cca565a7b2c631
Aravind Srivatsan <asrivatsan@paypal.com>
2016-03-01 16:40:13 +0530

    Modified title

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ var IndexModel = require('../models/index');
 module.exports = function(router) {
   router.get('/', function(req, res) {
     var info = {
-      title: 'Magical Me!',
+      title: 'magical.me',
       message: 'Well this could be an autobiography, but for now its just a portfolio'
     };
     res.render('index', new IndexModel(info));
